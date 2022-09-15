266e9c6342a380a7f744c1683871196bdf8a42ba
Helin Faidhan <helinkurd1997@gmail.com>
2022-09-15 19:36:35 +0300

    uncompleted

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+console.log(cats.push("Ralph"));
+console.log(cats.unshift("Bob"));
+console.log(cats.pop);
+console.log(cats.shift);
+console.log(cats);
+
+
+
+
